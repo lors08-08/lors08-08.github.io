b839d04a50e50859fd724e670d6078a63c862876
lors <lors080898@icloud.com>
2020-11-19 20:53:21 +0300

    edited build version

---
+++ chat-desktop/precache-manifest.dcc7941a1596414ea9eacf5327d2e665.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0f20d5db73a83b48aece21a54fe9a4e7",
+    "url": "./index.html"
+  },
+  {
+    "revision": "752168d3a73994b133af",
+    "url": "./static/css/2.764ccc25.chunk.css"
+  },
+  {
+    "revision": "0736f0a0f653fe9e6213",
+    "url": "./static/css/main.7b814778.chunk.css"
+  },
+  {
+    "revision": "752168d3a73994b133af",
+    "url": "./static/js/2.6e6ac23b.chunk.js"
+  },
+  {
+    "revision": "ba47d3d3182359165f569d7eb1374e54",
+    "url": "./static/js/2.6e6ac23b.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "0736f0a0f653fe9e6213",
+    "url": "./static/js/main.8484348c.chunk.js"
+  },
+  {
+    "revision": "ae1a1ad0a5e998fb0a00",
+    "url": "./static/js/runtime-main.ca1efca6.js"
+  }
+]);
--- chat-desktop/service-worker.js
+++ chat-desktop/service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.d37dcd70698d2820bfe5ce6340b708d8.js"
+  "./precache-manifest.dcc7941a1596414ea9eacf5327d2e665.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("./index.html"), {
   
   blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
 });
